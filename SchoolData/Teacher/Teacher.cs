61a14cc23421fa94759563873f4b0b299e83831c
EnriqueBiosca <enriquebiosca28@gmail.com>
2026-01-31 00:11:48 -0500

    Adding the teacher class

---
+++ SchoolData/Teacher/Teacher.cs
@@ -0,0 +1,23 @@
+﻿using domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace domain.Entities.Teacher
+{
+    public class Teacher : Entity
+    {
+        public string Name { get; private set; } = string.Empty;
+        public string LastName { get; private set; } = string.Empty;
+        public float Salary { get; private set; }
+
+        public Teacher(Guid id, string name, string lastName, float salary) : base(id)
+        {
+            Name = name;
+            LastName = lastName;
+            Salary = salary;
+        }
+    }
+}
